0ae8e330cfb258257e6b8ea2f523d16c3cae2659
Fizic <kirya.firsov.04@gmail.com>
2025-12-24 19:41:38 +0300

    fix: deploy

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="bg-white shadow-md sticky top-0 z-50">
     <div class="container mx-auto px-4">
       <div class="flex justify-between items-center h-16">
-        <NuxtLink to="/" class="text-2xl font-bold text-primary-600">
+        <NuxtLink to="herzen-7-sem-portfolio/" class="text-2xl font-bold text-primary-600">
           Портфолио
         </NuxtLink>
         <div class="flex space-x-6">
@@ -14,7 +14,7 @@
             Обо мне
           </NuxtLink>
           <NuxtLink
-            to="/works"
+            to="/herzen-7-sem-portfolio/works"
             class="text-gray-700 hover:text-primary-600 transition-colors font-medium"
             active-class="text-primary-600"
           >
